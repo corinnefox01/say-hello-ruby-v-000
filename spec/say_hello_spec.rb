9a02490fb8e9e06dea4c7760588a69cfb48c8578
Corinne Fox <corinnefox01@gmail.com>
2019-05-23 14:21:33 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -13,8 +13,10 @@ describe "say_hello" do
   end
 end
 
-def say_hello (name="Ruby Programmer!" )
-  puts Hello  #{name}
+def say_hello (name = "Ruby Programmer!" )
+  puts "Hello,  #{name}"
 end
+
+
 
 say_hello ("Ruby Programmer!")
